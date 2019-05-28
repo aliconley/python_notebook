247a61c0722db94d36417e0f0b0f003ed34a5108
aliconley <alirconley@gmail.com>
2019-05-28 14:55:28 -0400

    okay

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from datetime import date
-
-
-
 """
 Note Book app
 
@@ -10,16 +7,27 @@ ID
 Content
 Date/Time
 """
-
 counter = 1
 notebook = []
 now = date.today()
 
+while True:
+    user_response = input("Would you like to do? \n"
+                          "1. Add a the note? \n"
+                          "2. Print a note \n"
+                          "3. Exit\n>"
+        )
+    if user_response == '1':
+        content = input('what is the note \n>')
+        note_id = counter
+        note = (note_id, str(now), content)
+        notebook.append(note)
+        counter += 1
+    elif user_response == '2':
+        for note in notebook:
+            print(f"Id: {note[0]} {note[2]}")
+    elif user_response == '3':
+        exit()
+    else:
+        print('Invalid input')
 
-while counter <= 5:
-    content = input("what is the note? \n>")
-    note_id = counter
-    note = (note_id, str(now), content)
-    counter += 1
-    notebook.append(note)
-    print(notebook)
